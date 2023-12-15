5f52cf935cfd617a8f3a127bc7b53e480a017e63
ElhSabrina <150938222+ElhSabrina@users.noreply.github.com>
2023-12-15 15:00:54 +0100

    Update partie 2.py

--- partie 2.py
+++ partie 2.py
@@ -22,3 +22,123 @@ for elem in res:
     for val in elem:
         print(val, end="\t")
     print()
+
+def calcul_vecteur_tf_idf(question):
+    dico = {}
+    dico_idf = idf('cleaned')
+    dico_tf = tf_texte(question)
+    liste1 = tokenisation(question)
+    liste2 = recherche_mot_question(liste1, 'cleaned')
+
+    for val in liste1:
+        if val in liste2 and val in dico_idf and val in dico_tf:
+            dico[val] = dico_idf[val] * dico_tf[val]
+        else:
+            dico[val] = 0.0
+    return dico
+
+
+
+
+def produit_scalaire(A, B):
+    while len(A) != len(B):
+        A = []
+        input_A = input("Entrez la liste A séparée par des espaces : ")
+        for element in input_A.split():
+            A.append(float(element))
+
+        B = []
+        input_B = input("Entrez la liste B séparée par des espaces : ")
+        for element in input_B.split():
+            B.append(float(element))
+
+    produit_scalaire_resultat = 0
+
+    for i in range(len(A)):
+        produit_scalaire_resultat += A[i] * B[i]
+    return produit_scalaire_resultat
+
+
+
+def norme_vecteur(A):
+    norme = 0
+    for i in range(len(A)):
+        norme += A[i] ** 2
+    norme = sqrt(norme)
+    return norme
+
+
+def similarite(A, B):
+    produit_scalaire_resultat = produit_scalaire(A, B)
+    norme_A = norme_vecteur(A)
+    norme_B = norme_vecteur(B)
+
+    if norme_A == 0 or norme_B == 0:
+         return 0
+
+    similarite = produit_scalaire_resultat / (norme_A * norme_B)
+    return similarite
+
+
+def mots_tf_idf_eleves(question):
+    vecteur_tf_idf = calcul_vecteur_tf_idf(question)
+    maxi = 0
+    mots_max = []
+
+    for cle, elem in vecteur_tf_idf.items():
+        if elem > maxi:
+            maxi = elem
+            mots_max = [cle]
+        elif elem == maxi:
+            mots_max.append(cle)
+
+    return mots_max
+#test
+print(calcul_vecteur_tf_idf("hi guys la ecologie belle comment je suis sabrina"))
+print(produit_scalaire([1,2],[3,4]))
+print(norme_vecteur([1,2,3]))
+print(similarite([1,2,3],[3,4,5]))
+print(mots_tf_idf_eleves("hi guys je la ecologie suis sabrina"))
+
+def extraire_premiere_phrase_max_tf_idf(question):
+    doc = doc_pertinent(question)
+    mots_max_tf_idf = mots_tf_idf_eleves(question)
+
+    # Parcourir le document pour extraire la première phrase contenant l'un des mots
+    with open(f"cleaned/{doc}", "r", encoding='utf-8') as file:
+        for line in file:
+            phrases = line.split('.')
+            for phrase in phrases:
+                for mot in mots_max_tf_idf:
+                    if mot in phrase:
+                        premiere_phrase_trouvee = phrase.strip()
+                        break  #des qu'un premier mot est trouvé on stop la boucle
+                if premiere_phrase_trouvee:
+                    break  #des que la phrase est trouvée on stop l'autre boucle aussi
+
+    return premiere_phrase_trouvee
+
+def sous_chaine(str1,str2):
+    str1= tokenisation(str1)
+    str2=tokenisation(str2)
+    for mot in mots_str2:
+        if mot == str1:
+            return True
+    return False
+def ameliorer_reponse(question):
+    question = tokenisation(question)
+    premiere_phrase_trouvee = extraire_premiere_phrase_max_tf_idf(question)
+
+    question_starters = {
+        "Comment": "Après analyse, ",
+        "Pourquoi": "Car, ",
+        "Peux-tu": "Oui, bien sûr!"
+
+    }
+    mot_cle = question[0]
+    for elem in question_starters.keys():
+        if sous_chaine(mot_cle, elem) or sous_chaine(elem, mot_cle):
+            premiere_phrase_trouvee = question_starters[elem] + premiere_phrase_trouvee
+            break #dés qu'il y a une clé qui correspond au premier mot du dico on stoppe la boucle
+
+    return premiere_phrase_trouvee
