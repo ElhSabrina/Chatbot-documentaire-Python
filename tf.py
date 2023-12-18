c13191518ddfc13370f6b4941f39dfd50c37d929
lucygrs <150938254+lucygrs@users.noreply.github.com>
2023-12-19 00:36:48 +0100

    Update tf.py

--- tf.py
+++ tf.py
@@ -3,8 +3,8 @@ from occur import *
 def tf(fichier):
     """Cette fonction prend en parmètre un fichier et renvoie un dictionnaire associant à chaque mot son score TF. On
     admet que le score TF d'un mot est le quotient de son occurence sur le nombre de mot du fichier"""
-    with open(fichier,"r", encoding = 'utf-8') as file: # On ouvre le fichier en mode lecture
-        texte = file.read() # La variable texte est une chaine de caractère contenat le contenu du fichier file
+    with open(fichier,"r", encoding = 'utf-8') as f: # On ouvre le fichier en mode lecture
+        texte = f.read() # La variable texte est une chaine de caractère contenat le contenu du fichier f
         nb_mots = len(texte.split())
         dico_occur = occur(texte) # Ce dictionnaire associe à chaque mot son occurence dans le texte
         for cle, valeur in dico_occur.items():
@@ -19,4 +19,4 @@ def tf_texte(texte):
     dico_occur = occur(texte)
     for cle, valeur in dico_occur.items():
         dico_occur[cle]= valeur/nb_mots
-    return dico_occur
+    return dico_occu
